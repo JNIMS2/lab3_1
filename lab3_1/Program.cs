604cd54354eb6bfea556d6a2f2c7986d8f36cb12
Jessica Miller-Nims <Jessicamiller-nims@quickenloans.com>
2021-07-26 16:43:07 -0400

    setting up the beginning

--- lab3_1/Program.cs
+++ lab3_1/Program.cs
@@ -6,9 +6,16 @@ namespace lab3_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("practice for git hub");
-            Console.WriteLine("test");
+            //create 3 arrays and fill them with student info. name, fav food, previous title
+            string[] name = { "John", "Jacob", "Jingleheimer", "Schmidt" };
+            string[] favFood = { "Pizza", "Tacos", "Ramen", "Burgers" };//clearly these r college students
+            string[] title = { "Server", "Customer Service Representative", "Gym teacher", "Self-employed" };
+
+            Console.WriteLine("Welcome to our DevBuild class! Which student would you like to learn more about?");
+            Console.Write("Enter a number 1-4: ");
+            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine(input);
+
         }
     }
 }
