dd3e72f2611347264e852149e2a5d1cc56425362
Jessica Miller-Nims <Jessicamiller-nims@quickenloans.com>
2021-07-26 16:15:50 -0400

    trying it out

---
+++ lab3_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lab3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("practice for git hub");
+        }
+    }
+}
